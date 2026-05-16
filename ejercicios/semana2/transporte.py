a26a23f4533100c5fab34bdf8cf2755455013c6b
lalo_aguirre <1725111104@utectulancingo.edu.mx>
2026-05-16 13:12:51 -0600

    Create transporte.py

---
+++ ejercicios/semana2/transporte.py
@@ -0,0 +1,14 @@
+class Transporte:
+  def __init__ (self, marca, modelo, color, precio, numero_pasajeros, tipo_combustible,
+                numero_llantas, pais_origen, velocidad-max)
+  self. marca = marca
+  self. modelo = modelo
+  self. color = color
+  self. precio = precio
+  self. peso = peso
+  self. numero_pasajeros = numero_pasajeros
+  self. tipo_combustible = tipo_combistible
+  self. numero_llantas = numero_llantas
+  self. pais_origen = pais_origen
+  self. velocidad_max = velocidad_max
+  
