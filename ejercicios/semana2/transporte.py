6e506574ff669f34a0f9a097c541c887322739b8
lalo_aguirre <1725111104@utectulancingo.edu.mx>
2026-05-16 13:32:00 -0600

    Update transporte.py

--- ejercicios/semana2/transporte.py
+++ ejercicios/semana2/transporte.py
@@ -1,26 +1,26 @@
 class Transporte:
-  def __init__ (self, marca, modelo, color, precio, numero_pasajeros, tipo_combustible,
-                numero_llantas, pais_origen, velocidad-max)
-  self. marca = marca
-  self. modelo = modelo
-  self. color = color
-  self. precio = precio
-  self. peso = peso
-  self. numero_pasajeros = numero_pasajeros
-  self. tipo_combustible = tipo_combistible
-  self. numero_llantas = numero_llantas
-  self. pais_origen = pais_origen
-  self. velocidad_max = velocidad_max
-  print(f"Marca del coche:{self.marca}")
-  print(f"Nombre del modelo:{self.modelo}")
-  print(f"Color:{self.color}")
-  print(f"Precio:{self.precio}")
-  print(f"Peso en kilogramos:{self.peso}")
-  print(f"Número de pasajeros:{self.numero_pasajeros}")
-  print(f"Tipo de combustible:{self.tipo_combustible}")
-  print(f"Número de llantas:{self.numero_llantas}")
-  print(f"País de origen:{self.pais_origen}")
-  print(f"Velocidad máxima:{self.velocidad_max}")
+  def __init__ (self, marca, modelo, color, precio, peso, numero_pasajeros, tipo_combustible,
+                numero_llantas, pais_origen, velocidad_max):
+      self. marca = marca
+      self. modelo = modelo
+      self. color = color
+      self. precio = precio
+      self. peso = peso
+      self. numero_pasajeros = numero_pasajeros
+      self. tipo_combustible = tipo_combustible
+      self. numero_llantas = numero_llantas
+      self. pais_origen = pais_origen
+      self. velocidad_max = velocidad_max
+      print(f"Marca del coche:{self.marca}")
+      print(f"Nombre del modelo:{self.modelo}")
+      print(f"Color:{self.color}")
+      print(f"Precio:{self.precio}")
+      print(f"Peso en kilogramos:{self.peso}")
+      print(f"Número de pasajeros:{self.numero_pasajeros}")
+      print(f"Tipo de combustible:{self.tipo_combustible}")
+      print(f"Número de llantas:{self.numero_llantas}")
+      print(f"País de origen:{self.pais_origen}")
+      print(f"Velocidad máxima:{self.velocidad_max}")
   def avanzar(self):
     print(f"El transporte avanza")
   def frenar(self):
@@ -31,8 +31,8 @@ class Transporte:
     print(f"El transporte acelera")
   def gira(self):
     print(f"El tranporte gira")
-transporte1 = Transporte("Mercedes", "Sprinter", "Negro", "3500 kg.", 4, "Diésel", 4, "Alemania",
-                         "$800,000")
+transporte1 = Transporte("Mercedes", "Sprinter", "Negro", "$800,000", "3500 kg.", 4, "Diésel", 4, 
+                         "Alemania")
 transporte1.avanzar()
 transporte1.frenar()
 transporte1.encender()
