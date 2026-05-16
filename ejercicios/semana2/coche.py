71aa135b00851aef2a7f9529afc86fdbf91ece98
lalo_aguirre <1725111104@utectulancingo.edu.mx>
2026-05-16 15:27:01 -0600

    Create coche.py

---
+++ ejercicios/semana2/coche.py
@@ -0,0 +1,15 @@
+class Coche:
+  def __init__(self, marca, modelo, color, velocidad_max, tipo_combustible,
+               numero:puertas, transmision, cilindros, capacidad_maletero,
+               precio):
+                 self.marca = marca
+                 self.modelo = modelo
+                 self.color = color
+                 self. velocidad_max = velocidad_max
+                 self.tipo_combustible = tipo_combustible
+                 self.numero_puertas = numero_puertas
+                 self.transmisio = transmision
+                 self.cilindros = cilindros
+                 self. capacidad_maletero = capacidad_maletero
+                 self.precio = precio
+                 print(f"Marca del coche
