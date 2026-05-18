46865db124f08c6975b48244a6cff0cb16a7498c
lalo_aguirre <1725111104@utectulancingo.edu.mx>
2026-05-18 17:59:35 -0600

    Update personaje_rpg.py

--- ejercicios/semana2/personaje_rpg.py
+++ ejercicios/semana2/personaje_rpg.py
@@ -5,10 +5,11 @@ class PersonajeRpg:
         self.raza = raza
         self.equipamiento = equipamiento
         self.altura = altura
-        self.velocidad = velocidad
+        self.fuerza = fuerza
         self.velocidad = velocidad
         self.idioma = idioma
         self.inteligencia = inteligencia
+        self.edad = edad
         self.nombre_apodo = nombre_apodo
         print(f"Género:{self.genero}")
         print(f"Raza:{self.raza}")
@@ -18,21 +19,21 @@ class PersonajeRpg:
         print(f"Velocidad:{self.velocidad}")
         print(f"Idioma:{self.idioma}")
         print(f"Inteligencia:{self.inteligencia}")
-        print(f"Edad.{self.edad}")
+        print(f"Edad:{self.edad}")
         print(f"Nombre o apodo:{self.nombre_apodo}")
     def correr (self):
         print(f"Está corriendo")
     def saltar (self):
         print(f"Está saltando")
     def golpear (self):
-        print(f"Está saltando")
+        print(f"Está golpeando")
     def comer (self):
         print(f"Está comiendo")
     def agacharse (self):
         print(f"Está agachándose")
 
 Lalo = PersonajeRpg("Hombre", "Asiática", "2 pistolas", "32 pixeles", 
-                    "99/999", "6 casillas por turno", "Español", "Alta"
+                    "99/999", "6 casillas por turno", "Español", "Alta",
                     20, "Lalo")
 Lalo.correr()
 Lalo.saltar()
