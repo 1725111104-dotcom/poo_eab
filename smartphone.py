30d5caf356d835de14e53d173c3ba2464e758c08
lalo_aguirre <1725111104@utectulancingo.edu.mx>
2026-05-19 16:04:09 +0000

    SMARTPHONE .py

---
+++ smartphone.py
@@ -0,0 +1,42 @@
+class Smartphone:
+    def __init__ (self, tamano, peso, forma, procesador, memoria_ram,
+                  resolucion_camaras, capacidad_bateria, almacenamiento,
+                  tamano_pantalla, numero_botones):
+        self.tamano = tamano
+        self.peso = peso
+        self.forma = forma
+        self.procesador = procesador
+        self.memoria_ram = memoria_ram
+        self.resolucion_camaras = resolucion_camaras
+        self.capacidad_bateria = capacidad_bateria
+        self.almacenamiento = almacenamiento
+        self.tamano_pantalla = tamano_pantalla
+        self.numero_botones = numero_botones
+        print(f"Tamaño del teléfono: {self.tamano}")
+        print(f"Peso del smartphone:{self.peso}")
+        print(f"Forma del smartphone:{self.forma}")
+        print(f"Nombre del procesador:{self.procesador}")
+        print(f"Capacidad de la memoria RAM:{self.memoria_ram}")
+        print(f"Resolución de la cámara principal:{self.resolucion_camaras}")
+        print(f"Capacidad de la batería:{self.capacidad_bateria}")
+        print(f"Capacidad de almacenamiento:{self.almacenamiento}")
+        print(f"Tamaño de la pantalla:{self.tamano_pantalla}")
+        print(f"Número de botones:{self.numero_botones}")
+
+    def llamar (self):
+        print("Esta llamando")
+    def mensajes (self):
+        print("Esta mensajeando")
+    def juegos (self):
+        print("Esta jugando")
+    def fotos (self):
+        print("Esta tomando fotos")
+    def videos (self):
+        print("Esta videando")
+S30 = Smartphone(9.1, 150, "Rectangular", "Snapdragon 8 gen", 16, 200,
+                 4000, 1000, 8.8, 3)
+S30.llamar()
+S30.mensajes()
+S30.juegos()
+S30.fotos()
+S30.videos()
