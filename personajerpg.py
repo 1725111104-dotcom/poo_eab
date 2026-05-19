1bef067c65b60f9d0487740a3f06db2ecf664eb6
lalo_aguirre <1725111104@utectulancingo.edu.mx>
2026-05-19 16:09:27 +0000

    PERSONAJERPG .py

---
+++ personajerpg.py
@@ -0,0 +1,42 @@
+class PersonajeRpg:
+    def __init__(self, genero, raza, equipamiento, altura, fuerza,
+                 velocidad, idioma, inteligencia, edad, nombre_apodo):
+        self.genero = genero
+        self.raza = raza
+        self.equipamiento = equipamiento
+        self.altura = altura
+        self.fuerza = fuerza
+        self.velocidad = velocidad
+        self.idioma = idioma
+        self.inteligencia = inteligencia
+        self.edad = edad
+        self.nombre_apodo = nombre_apodo
+        print(f"Género:{self.genero}")
+        print(f"Raza:{self.raza}")
+        print(f"Equipamiento:{self.equipamiento}")
+        print(f"Altura:{self.altura}")
+        print(f"Fuerza:{self.fuerza}")
+        print(f"Velocidad:{self.velocidad}")
+        print(f"Idioma:{self.idioma}")
+        print(f"Inteligencia:{self.inteligencia}")
+        print(f"Edad:{self.edad}")
+        print(f"Nombre o apodo:{self.nombre_apodo}")
+    def correr (self):
+        print(f"Está corriendo")
+    def saltar (self):
+        print(f"Está saltando")
+    def golpear (self):
+        print(f"Está golpeando")
+    def comer (self):
+        print(f"Está comiendo")
+    def agacharse (self):
+        print(f"Está agachándose")
+
+Lalo = PersonajeRpg("Hombre", "Asiática", "2 pistolas", "32 pixeles", 
+                    "99/999", "6 casillas por turno", "Español", "Alta",
+                    20, "Lalo")
+Lalo.correr()
+Lalo.saltar()
+Lalo.golpear()
+Lalo.comer()
+Lalo.agacharse()
